b545311e764f889f85eee6130d91775cdad8c642
Andre Dsouza <andredsouza256@gmail.com>
2026-08-02 00:24:48 +0530

    chore: rename application to com.spineleaf.app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.material_3_demo"
+    namespace = "com.spineleaf.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.2.13676358"
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.material_3_demo"
+        applicationId = "com.spineleaf.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
